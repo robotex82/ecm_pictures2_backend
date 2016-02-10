3c4c46f53ab0fd45d33865fb8de16c61f75c82ab
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-02-10 14:20:19 +0100

    Fixed parameter handling for base64 images.

--- app/controllers/ecm/pictures/backend/pictures_controller.rb
+++ app/controllers/ecm/pictures/backend/pictures_controller.rb
@@ -6,11 +6,10 @@ class Ecm::Pictures::Backend::PicturesController < Itsf::Backend::Resource::Base
   private
 
   def request_includes_base64_image?
-    params[:ecm_pictures_picture].key?(:image_base64) && params[:ecm_pictures_picture][:image_base64].present?
+    params[:ecm_pictures_picture].key?(:image_base64)
   end
 
-  def extract_base64
-    encoded_image = params[:ecm_pictures_picture].delete(:image_base64)
+  def extract_image_base64(encoded_image)
     decoded_image = Base64.decode64(encoded_image.gsub(/^data\:image\/\w+\;base64\,/, '')).force_encoding('UTF-8')
     content_type = encoded_image.split(';').first.split(':').last
     basename = 'image'
@@ -23,9 +22,13 @@ class Ecm::Pictures::Backend::PicturesController < Itsf::Backend::Resource::Base
   end
 
   def permitted_params
-    permitted_params = params
-                       .require(:ecm_pictures_picture)
-                       .permit(:picture_gallery_id, :name, :markup_language, :description, :image, :tag_list)
-    permitted_params.merge(image: extract_base64) if request_includes_base64_image?
+    processed_params = params.deep_dup
+    image_base64 = processed_params[:ecm_pictures_picture].try(:delete, :image_base64)
+    if image_base64.present?
+      image = extract_image_base64(image_base64) 
+      processed_params.merge!(image: extract_base64)
+    end
+    processed_params.require(:ecm_pictures_picture)
+                      .permit(:picture_gallery_id, :name, :markup_language, :description, :image, :tag_list)
   end
 end
